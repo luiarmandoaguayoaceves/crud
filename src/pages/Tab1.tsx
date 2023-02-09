6ef55633e3c4ff009b4292f825038f723a17a1a7
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2023-02-09 07:00:52 -0600

    lista

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,62 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import React, { useState, useEffect } from 'react';
+import {
+  IonContent,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+  IonList,
+  IonItem,
+  IonAvatar,
+  IonLabel,
+  IonInput,
+} from '@ionic/react';
+import { IonButton, IonIcon } from '@ionic/react';
+import { star, create, trash } from 'ionicons/icons';
 
-const Tab1: React.FC = () => {
+function Example() {
+  const [items, setItems] = useState<string[]>([]);
+
+  useEffect(() => {
+    const newItems = [];
+    for (let i = 1; i < 51; i++) {
+      newItems.push(`Item ${items.length + i}`);
+    }
+    setItems([...items, ...newItems]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const [nombre, setNombre] = useState([])
+
+  useEffect(() => {
+    const getNames = () =>{
+      fetch('localhost:3306')
+    }
+  }, [])
+  
+
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
+    <IonContent>
+      <IonList>
+      <IonItem>
+        <IonLabel>Nombre</IonLabel>
+        <IonInput clearInput={true} placeholder="Enter text"></IonInput>
+      </IonItem>
+        {items.map((item, index) => (
+          <IonItem key={item}>
+            <IonAvatar slot="start">
+              
+              <img src={'https://picsum.photos/80/80?random=' + index} alt="avatar" />
+            </IonAvatar>
+            <IonLabel>{item}
+            <IonIcon slot="start" icon={create}></IonIcon>
+            <IonIcon slot="start" icon={trash}></IonIcon>
+            </IonLabel>
+          </IonItem>
+        ))}
+      </IonList>
+      <IonInfiniteScroll>
+        <IonInfiniteScrollContent loadingText="Please wait..." loadingSpinner="bubbles"></IonInfiniteScrollContent>
+      </IonInfiniteScroll>
+    </IonContent>
   );
-};
-
-export default Tab1;
+}
+export default Example;
